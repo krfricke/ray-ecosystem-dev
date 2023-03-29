68e80fdb6e8ebf27f4b0af64fdbe47586810daca
Kai Fricke <kai@anyscale.com>
2023-03-29 12:20:06 -0700

    no sudo

--- install_docker.sh
+++ install_docker.sh
@@ -5,10 +5,10 @@ export DOCKER_HOST=tcp://docker:2376
 export DOCKER_TLS_VERIFY=1
 export DOCKER_CERT_PATH=/certs/client
 
-sudo apt-get update -qq && apt-get upgrade -qq
-sudo apt-get install -y -qq \
+apt-get update -qq && apt-get upgrade -qq
+apt-get install -y -qq \
     curl python-is-python3 git build-essential \
     sudo unzip unrar apt-utils dialog tzdata wget rsync \
     language-pack-en tmux
 
-sudo curl -o- https://get.docker.com | sh
+curl -o- https://get.docker.com | sh
